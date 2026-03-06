ead391653db1df91c4fa84f80ebfec11c420921f
parkerfletcher1 <parker.fletcher1@gmail.com>
2026-03-05 19:10:42 -0600

    updated
    made test file fixed tokenizer bug

--- Tokenizer.java
+++ Tokenizer.java
@@ -2,14 +2,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Tokenizer {
+
+    private HashMap<String, Integer> tokenFrequency = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
+        /* checks arg length to ensure an argumement is provided */
         if (args.length == 0) {
             System.out.println("Please provide a filePath.");
             return;
         }
 
+        /* checks if the provided path is a directory */
         File Dir = new File(args[0]);
         if (!Dir.isDirectory()) {
             System.out.println("The provided path is not a directory.");
@@ -18,9 +24,22 @@ public class Tokenizer {
 
         Tokenizer tokenizer = new Tokenizer();
         tokenizer.tokenize(Dir);
+        tokenizer.printTokenFrequencies(tokenizer.tokenFrequency);
 
     }
 
+    // method to print the token frequencies
+    private void printTokenFrequencies(HashMap<String, Integer> tokenFrequency) {
+        System.out.println("Token Frequencies:");
+        for (String token : tokenFrequency.keySet()) {
+            System.out.printf("%s: %d%n", token, tokenFrequency.get(token));
+        }
+    }
+
+    /*
+     * tokenizes the files in the provided directory and prints the tokens to the
+     * console
+     */
     private void tokenize(File dir) throws IOException {
         File[] files = dir.listFiles();
         if (files == null) {
@@ -34,19 +53,18 @@ public class Tokenizer {
                 String line = "";
                 BufferedReader br = new BufferedReader(new FileReader(file));
                 while ((line = br.readLine()) != null) {
-                    String[] tokens = line.toLowerCase().replaceAll("[^a-zA-Z0-9\\s]+", "").split("\\s+");
+                    // Convert to lowercase, remove punctuation, and split into tokens using regex.
+                    String[] tokens = line.toLowerCase().replaceAll("[^a-zA-Z0-9\\s]+", "").split("");
                     for (String token : tokens) {
-                        System.out.println(token);
+                        if(!token.isEmpty()) {
+                            tokenFrequency.put(token, tokenFrequency.getOrDefault(token, 0) + 1);
+                        }
                     }
                 }
                 br.close();
-                System.out.println("Tokenizing file: " + file.getName());
-
             } else if (file.isDirectory()) {
                 // Recursively tokenize subdirectories
                 tokenize(file);
             }
         }
-
-    }
-}
+}}
